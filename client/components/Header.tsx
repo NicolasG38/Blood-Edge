e4216961b1feaa0f7cc584ca655956c852f8e2b0
Nicolas GIRARD <NG38@outlook.fr>
2025-07-23 19:06:11 +0200

    création du composant permettant à l'utilisateur de se connecter

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-
+import Link from "next/link";
 import SignupLoginBtn from "../uiux/SignUpLoginBtn";
 import Image from "next/image";
 
@@ -10,14 +10,18 @@ export default function Header() {
 				STELLAR BLADE© 2024 SHIFT UP Corporation. All right reserved. Published
 				by Sony Interactive Entertainment
 			</p>
-			<Image
-				id="logo"
-				src="/assets/logo/Blood-Edge_V1.webp"
-				alt="Logo"
-				width={188}
-				height={150}
-			/>
-			<SignupLoginBtn />
+			<Link href="/">
+				<Image
+					id="logo"
+					src="/assets/logo/Blood-Edge_V1.webp"
+					alt="Logo"
+					width={188}
+					height={150}
+				/>
+			</Link>
+			<Link href="/signup&login">
+				<SignupLoginBtn />
+			</Link>
 		</header>
 	);
 }
---
+++ client/src/app/(pages)/signup&login/page.tsx
@@ -0,0 +1,15 @@
+import Header from "../../../../components/Header";
+import Footer from "../../../../components/Footer";
+import Login from "../../../../components/Login";
+
+export default function SignUpLogin() {
+	return (
+		<>
+			<Header />
+			<main>
+				<Login />
+			</main>
+			<Footer />
+		</>
+	);
+}
