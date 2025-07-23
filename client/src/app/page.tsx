406df4cb9c744ff774e8f0ca56697d41206a8202
Nicolas GIRARD <NG38@outlook.fr>
2025-07-23 13:23:05 +0200

    création du carrousel, la position absolute du header a dû être supprimé ce qui provoque un espacement non désiré

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Carrousel from "../../components/Carrousel";
 
 export default function Homepage() {
 	return (
 		<>
 			<Header />
+			<Carrousel />
 			<Footer />
 		</>
 	);
