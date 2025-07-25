8c2c2c81fa53d8d2af51027ae5308af659f76535
Nicolas GIRARD <NG38@outlook.fr>
2025-07-25 07:42:01 +0200

    mise en place du JWT, de la vérification du mot de passe par Argon2 et de la logique de connexion au compte, création d'un bouton de déconnexion avec suppression du token

--- server/src/modules/JWT.js
+++ server/src/modules/JWT.js
@@ -1,10 +1,8 @@
 import jwt from "jsonwebtoken";
 
-const token = jwt.sign(
-	{ sub: user.id.toString() }, // payload
-	process.env.JWT_SECRET, // clé secrète
-	{ expiresIn: "1h" }, // durée de validité
-);
+const token = jwt.sign({ sub: user.id.toString() }, process.env.JWT_SECRET, {
+	expiresIn: "1h",
+});
 
 const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
