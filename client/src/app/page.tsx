89f6c9a781cba246345a33ece42529c368a6c41c
Nicolas GIRARD <NG38@outlook.fr>
2025-08-28 09:48:14 +0200

    correction d'un problème d'hydratation sur le composant searchBar. correction du CSS sur StatsEVE

---
+++ client/src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Header from "../../components/0_Home/Header";
+import Footer from "../../components/0_Home/Footer";
+import Carrousel from "../../components/0_Home/Carrousel";
+import Modal from "../../components/0.1_Auth/Modal";
+import SectionBtn from "../../uiux/SectionBtn";
+import Login from "../../components/0.1_Auth/Login";
+import SubSection from "../../uiux/SubSection";
+
+import { useState } from "react";
+import LocationsBtn from "../../uiux/LocationsBtn";
+
+export default function Homepage() {
+	const [openLogin, setOpenLogin] = useState(false);
+	return (
+		<>
+			<Header />
+			<main className="parameter">
+				<Modal open={openLogin} onClose={() => setOpenLogin(false)}>
+					<Login
+						onSuccess={() => setOpenLogin(false)}
+						onSwitch={() => setOpenLogin(false)}
+					/>
+				</Modal>
+				<Carrousel />
+				<LocationsBtn />
+				<SectionBtn />
+				<SubSection />
+			</main>
+			<Footer />
+		</>
+	);
+}
